659d1de3bdbf62ee25fa2320863d1e16506d4bbe
vishnuvardhansunkara <vishnusunkara17@gmail.com>
2024-01-16 15:37:44 -0500

    message

--- vars/runContainer.groovy
+++ vars/runContainer.groovy
@@ -1,3 +1,3 @@
 def call(){
-    sh "sudo docker run -d --name youtube1 -p 3000:3000 vishnusunkara/youtube:latest"
+    sh "docker run -d --name youtube1 -p 3000:3000 vishnusunkara/youtube:latest"
 }
